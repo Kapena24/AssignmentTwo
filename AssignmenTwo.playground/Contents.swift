779330459f4a7d29aec9fb79e354372f2696fb41
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-08-29 14:41:07 -1000

    Parts 1-8 In class Parts 9 & 10 on my own

---
+++ AssignmenTwo.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+/*var myFirstvariable = "I've created my first variable"
+// var is only used when declaring the variable (container)
+ var myFirstVariable: String
+
+//myFirstVariable = "I've created my firsr variable,"
+//myFirstvariable = "5"
+// QM is key for String, without it becomes intager
+
+var mySecondVariable = " oh and look I added 2 strings together."
+//Sring concatenation
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+var firstNewVariable = myFirstVariable + mySecondVariable + myThirdVariable
+
+print(myThirdVariable)*/
+
+var theFirstNewVariable = "My brian really hurts,"
+
+var theSecondNewVarivable = "but I'm doing my best."
+
+var theThirdNewVarivable = theFirstNewVariable + theSecondNewVarivable + "I think I've got that hang of it"
+
+var finalNewVarivable = theThirdNewVarivable
+
+print(finalNewVarivable)
